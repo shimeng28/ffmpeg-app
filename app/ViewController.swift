d701244eee10873de3a97fe7b2de50e1d883507a
shimeng.eric <shimeng.eric@bytedance.com>
2020-07-18 12:54:56 +0800

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by shimeng on 2020/7/18.
+//  Copyright © 2020 shimeng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
